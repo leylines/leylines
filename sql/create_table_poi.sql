88d28c60b451b5119f4cb70ac459d2a3c79e842d
Joerg Roth <info@leylines.ch>
2016-06-16 14:03:11 +0200

    Prepare for major database consolidation

--- sql/create_table_poi.sql
+++ sql/create_table_poi.sql
@@ -1,16 +1,17 @@
-SET @schemaName = 'poi';
-SET @tableName = 'test';
-SET @objectType = 4;
+SET @schemaName = 'leylines';
+SET @tableName = 'poi';
 
+SET @maintable = @schemaName + '.' + @tableName;
+SET @relationtable = @schemaName + '.' + @tableName + '_' + 'object_type';
 SET @pkey =  @tableName + '_pkey';
-SET @gix = @tableName + '_gix';
+SET @gidx = @tableName + '_gidx';
+SET @pidx = @tableName + '_pidx';
 
-CREATE TABLE @schemaName.@tableName
+CREATE TABLE @maintable
 (
-  gid serial NOT NULL,
+  site_id serial NOT NULL,
   site text,
   description text,
-  object_type integer references object_types(id) DEFAULT @objectType,
   object_sub_type integer references object_sub_types(id) DEFAULT 1,
   bovis_units integer DEFAULT 0,
   data_owner text,
@@ -23,37 +24,82 @@ CREATE TABLE @schemaName.@tableName
   date_creation timestamp without time zone,
   date_modification timestamp without time zone,
   country text,
-  CONSTRAINT @pkey PRIMARY KEY (gid)
+  CONSTRAINT @pkey PRIMARY KEY (site_id)
 )
 WITH (
   OIDS=FALSE
 );
 
-ALTER TABLE @schemaName.@tableName
+ALTER TABLE @maintable
   OWNER TO postgres;
-GRANT SELECT ON TABLE @schemaName.@tableName TO public;
-GRANT ALL ON TABLE @schemaName.@tableName TO postgres;
+GRANT SELECT ON TABLE @maintable TO public;
+GRANT ALL ON TABLE @maintable TO postgres;
 
-CREATE INDEX @gix
-  ON @schemaName.@tableName
+CREATE INDEX @pidx
+  ON @maintable
+  (site);
+
+CREATE INDEX @gidx
+  ON @maintable
   USING gist
   (geom);
 
-CREATE TRIGGER "05_trigger_get_coords"
+CREATE TRIGGER "01_trigger_date_update"
   BEFORE INSERT OR UPDATE
-  ON @schemaName.@tableName
+  ON @maintable
   FOR EACH ROW
-  EXECUTE PROCEDURE get_coords();
+  EXECUTE PROCEDURE date_update();
 
-CREATE TRIGGER "10_trigger_update_geom"
+CREATE TRIGGER "05_trigger_point_geom_get_coords"
   BEFORE INSERT OR UPDATE
-  ON @schemaName.@tableName
+  ON @maintable
   FOR EACH ROW
-  EXECUTE PROCEDURE update_point_geom();
+  EXECUTE PROCEDURE point_geom_get_coords();
 
-CREATE TRIGGER "20_trigger_get_country"
+CREATE TRIGGER "10_trigger_point_geom_update"
   BEFORE INSERT OR UPDATE
-  ON @schemaName.@tableName
+  ON @maintable
   FOR EACH ROW
-  EXECUTE PROCEDURE get_country();
+  EXECUTE PROCEDURE point_geom_update();
 
+CREATE TRIGGER "20_trigger_point_geom_get_country"
+  BEFORE INSERT OR UPDATE
+  ON @maintable
+  FOR EACH ROW
+  EXECUTE PROCEDURE point_geom_get_country();
+
+CREATE TABLE @relationtable  ( 
+  site_id int4 NOT NULL,
+  object_id int4 NOT NULL,
+  date_creation timestamp without time zone,
+  date_modification timestamp without time zone,
+  PRIMARY KEY(site_id,object_id)
+)
+WITH (
+  OIDS=FALSE
+);
+
+ALTER TABLE @relationtable
+  OWNER TO postgres;
+GRANT SELECT ON TABLE @relationtable TO public;
+GRANT ALL ON TABLE @relationtable TO postgres;
+
+ALTER TABLE @relationtable
+  ADD CONSTRAINT site_id_fkey
+  FOREIGN KEY (site_id)
+  REFERENCES @maintable(site_id)
+  ON UPDATE CASCADE
+  ON DELETE RESTRICT;
+
+ALTER TABLE @relationtable
+  ADD CONSTRAINT object_id_fkey
+  FOREIGN KEY (object_id)
+  REFERENCES object_type(object_id)
+  ON UPDATE CASCADE
+  ON DELETE RESTRICT;
+  
+CREATE TRIGGER "01_trigger_date_update"
+  BEFORE INSERT OR UPDATE
+  ON @relationtable
+  FOR EACH ROW
+  EXECUTE PROCEDURE date_update();
